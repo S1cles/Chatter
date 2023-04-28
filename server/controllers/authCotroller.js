37f7ebe0b246dfda794bd39ff0dd3b94baa9ca2a
TOKYO <nezexmain@gmail.com>
2023-04-28 07:00:08 +0200

    replace bcrypt

--- server/controllers/authCotroller.js
+++ server/controllers/authCotroller.js
@@ -1,43 +1,40 @@
 const User = require("../models/userModel")
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const path = require('path')
 const fs = require('fs')
 
 module.exports.register = async (req, res, next) => {
-    const { name, password, email } = req.body
-    console.log(req.body)
-    const isEmailExists = await User.findOne({ email: email })
-    const isNameExists = await User.findOne({ name: name })
+    const { name, password, email } = req.body;
+    console.log(req.body);
+    const isEmailExists = await User.findOne({ email: email });
+    const isNameExists = await User.findOne({ name: name });
 
     if (isEmailExists || isNameExists) {
-        return res.status(400).json({ message: 'User exists' })
+        return res.status(400).json({ message: 'User exists' });
     } else {
         try {
-            let salt = await bcrypt.genSalt(10)
-
-            const newPassword = password.toString();
-            const hashedPassword = await bcrypt.hash(newPassword, salt);
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
             User.create({
                 name: req.body.name,
                 email: email,
-                password: hashedPassword
-            })
-            return res.status(200).json({ message: 'Reg success' })
+                password: hashedPassword,
+            });
+            return res.status(200).json({ message: 'Reg success' });
         } catch (error) {
-            console.log(error)
-            return res.status(500).json({ message: 'Fail Reg' })
+            console.log(error);
+            return res.status(500).json({ message: 'Fail Reg' });
         }
     }
-}
+};
 
 module.exports.login = async (req, res, next) => {
     const { password, email } = req.body;
 
     const user = await User.findOne({ email: email }).lean();
 
-
-    console.log(user)
+    console.log(user);
 
     if (user) {
         try {
@@ -45,7 +42,7 @@ module.exports.login = async (req, res, next) => {
             if (isPasswordMatch) {
                 delete user.password;
                 const payload = {
-                    user: user
+                    user: user,
                 };
                 const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
                 return res.status(200).json({ message: 'Login success', token: token, isAuth: true });
@@ -96,29 +93,29 @@ module.exports.upload = async (req, res) => {
 
 module.exports.password = async (req, res, next) => {
     try {
-      const email = req.body.email;
-      const newPassword = req.body.newPassword;
-      console.log(newPassword + email);
-  
-      if (!newPassword || !email) {
-        return res.status(500).json('Something WRONG');
-      }
-  
-      const salt = await bcrypt.genSalt();
-      const hashPassword = await bcrypt.hash(newPassword, salt);
-  
-      const user = await User.findOneAndUpdate(
-        { email: email },
-        { password: hashPassword }
-      );
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      res.status(200).json({ message: 'Password updated' });
+        const email = req.body.email;
+        const newPassword = req.body.newPassword;
+        console.log(newPassword + email);
+
+        if (!newPassword || !email) {
+            return res.status(500).json('Something WRONG');
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(newPassword, salt);
+
+        const user = await User.findOneAndUpdate(
+            { email: email },
+            { password: hashPassword }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'Password updated' });
     } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Something wrong' });
+        console.log(error);
+        res.status(500).json({ message: 'Something wrong' });
     }
-  };
+};
